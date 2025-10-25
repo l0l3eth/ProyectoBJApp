777446f256b9dcf75a9c3893036214048db1f053
Milllll000 <156021494+Milllll000@users.noreply.github.com>
2025-10-25 08:29:29 -0600

    Forzar modo claro sin importar config del celular

--- app/src/main/java/mx/tec/proyectoBJ/ui/theme/Theme.kt
+++ app/src/main/java/mx/tec/proyectoBJ/ui/theme/Theme.kt
@@ -1,5 +1,6 @@
 package mx.tec.proyectoBJ.ui.theme
 
+import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -8,7 +9,11 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -34,19 +39,22 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun PtoyectoBJTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    // Los parámetros darkTheme y dynamicColor ya no son necesarios aquí.
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+    // Simplemente asignamos el esquema de colores claros.
+    val colorScheme = LightColorScheme
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    // Este bloque es para controlar el color de la barra de estado.
+    // Lo ajustamos para que siempre sea compatible con un tema claro.
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb() // Color de la barra de estado
+            // true: le dice al sistema que los iconos de la barra de estado (hora, batería) deben ser oscuros.
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = true
+        }
     }
 
     MaterialTheme(
